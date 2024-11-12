d726f172e94c3bbdfd990a3828a33a17cadf3c16
Andreina Oliveira <andreinaholiveira@gmail.com>
2024-11-12 18:56:32 -0400

    feat: test options

--- helper/options.js
+++ helper/options.js
@@ -4,52 +4,47 @@ const thresholds = {
   http_req_failed: ['rate<0.01']
 }
 
-const smoke = {
-  vus: 1, 
-  duration: '1m',
-  rps: rps,
-  thresholds: thresholds
-};
+const testOptions = {
+  somoke: {
+    vus: 1,
+    duration: '1m',
+    rps: rps,
+    thresholds: thresholds
+  },
 
-const performance = {
-  vus: 5, 
-  duration: '2m',
-  rps: rps,
-  thresholds: thresholds
-};
+  performance: {
+    vus: 5,
+    duration: '2m',
+    rps: rps,
+    thresholds: thresholds
+  },
 
-const load = {
-  stages: [
-    {duration: '1m', target: 5},
-    {duration: '2m', target: 5},
-    {duration: '1m', target: 0}
-  ],
-  rps: rps,
-  thresholds: thresholds
-};
+  load: {
+    stages: [
+      { duration: '1m', target: 5 },
+      { duration: '2m', target: 5 },
+      { duration: '1m', target: 0 }
+    ],
+    rps: rps,
+    thresholds: thresholds
+  },
 
-const stress = {
-  stages: [
-    {duration: '1m', target: 5},
-    {duration: '2m', target: 10},
-    {duration: '2m', target: 15},
-    {duration: '2m', target: 20},
-    {duration: '3m', target: 0}
-  ],
-  rps: rps,
-  thresholds: thresholds
-};
+  stress: {
+    stages: [
+      { duration: '1m', target: 5 },
+      { duration: '2m', target: 10 },
+      { duration: '2m', target: 15 },
+      { duration: '2m', target: 20 },
+      { duration: '3m', target: 0 }
+    ],
+    rps: rps,
+    thresholds: thresholds
+  }
+}
 
-const optionsType = __ENV.TEST || 'smoke'; 
+
+const selectedTest = __ENV.TEST || 'smoke';
 
 export function handleOptions() {
-  switch (optionsType) {
-    case 'smoke': return smoke;
-    case 'performance': return performance;
-    case 'load': return load;
-    case 'stress': return stress;
-    default:
-      console.error(`Unknown options type: ${optionsType}`);
-      process.exit(1);
-  }
+  return testOptions.selectedTest
 }
